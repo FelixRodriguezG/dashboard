dd1ddc64a4a4f3142e09cb080e6a18197a87c590
FelixRG <correofrg@gmail.com>
2024-03-16 20:55:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+    return (
+        <div className=" min-h-screen grid  grid-cols-6">
+            <div className=" bg-green-300 col-span-1">hola1</div>
+            <div className=" bg-blue-400 col-span-5">hola2</div>
+        </div>
+    )
+}
+
+export default App
